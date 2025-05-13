c2460a3056b8fc05f85567f1b9563a1a4d70b3b4
“kimkimkimbo” <booooa.kim@gmail.com>
2025-05-13 16:01:54 +0900

    기본 프로젝트

---
+++ templates/models/Bookmark.py
@@ -0,0 +1,9 @@
+"""
+bookmark
+{
+  "_id": ObjectId,
+  "user_id": ObjectId,
+  "post_id": ObjectId,
+  "created_at": ISODate
+}
+"""
---
+++ templates/models/Post.py
@@ -0,0 +1,11 @@
+"""
+post
+{
+  "_id": ObjectId,
+  "title": "게시물 제목",
+  "user_id": ObjectId,  // 작성자
+  "content": "글 내용",
+  "address": “영문로 55
+}
+
+"""
