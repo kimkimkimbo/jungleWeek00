4ab65ac0c5deab25fd922b1fe0e191944980bc95
“kimkimkimbo” <booooa.kim@gmail.com>
2025-05-14 02:41:35 +0900

    회원가입과 로그인/회원가입 페이지 연결

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 """
 """
+#SSR 방식이라면 jsonify() 대신 redirect()나 render_template()를 써야 함
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
+from models.user import create_user
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app) #모든 출처 허용, 매우 위험! 사용하지 않는 게 좋지만 당장 개발 편의를 위해 사용
 
 
 client = MongoClient('mongodb+srv://week00:250512@cluster0.vsudbri.mongodb.net/')
@@ -17,9 +22,39 @@ def home():
     return render_template('mainpage.html')
 
 
+
 #회원가입
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        #회원가입 사용자에게 입력받은 데이터를 DB에 저장
+        user_id = request.form['user_id']
+        name = request.form['name']
+        pw = request.form['pw']
+        
+        #회원가입 페이지에서 입력받은 데이터가 비어있지 않은지 확인
+        if not user_id or not name or not pw:
+            return render_template('signup.html', error='모두 입력해주세요.')
+    
+        create_user(db, user_id, name, pw)
+        
+        #SSR 방식이라면 jsonify() 대신 redirect()나 render_template()를 써야 함
+        #redirect는 사용자가 요청한 URL로 리다이렉트하는 함수
+        return redirect(url_for('login'))  # 회원가입 후 로그인 페이지로 이동
+    
+    else :
+        return render_template('signup.html')
+    
+    
+
 
 #로그인
+@app.route('/login', methods=['GET','POST'])
+def login():
+    
+    return render_template('login.html')
+
+
 
 #게시물
 
--- models/user.py
+++ models/user.py
@@ -7,3 +7,31 @@ Users
 }
 
 """
+#사용자 정보를 데이터베이스에 저장하는 함수
+def create_user(db, user_id, name, pw):
+    """
+    db: MongoDB 데이터베이스 객체
+    name: 사용자 이름
+    pw: 사용자 비밀번호
+    return: 생성된 사용자 ID
+    post_id: 즐겨찾기 게시물 아이디
+    """
+    user = {
+        "user_id": name,  # 사용자 ID는 이름으로 설정
+        "name": name,
+        "pw": pw,
+    }
+
+    result = db.users.insert_one(user)
+    return str(result.inserted_id)
+  
+
+#사용자 정보를 데이터베이스에서 조회하는 함수
+def get_user(db, user_id):
+    """
+    db: MongoDB 데이터베이스 객체
+    user_id: 사용자 ID
+    return: 사용자 정보 (딕셔너리 형태)
+    """
+    user = db.users.find_one({"_id": user_id})
+    return user
