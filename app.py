50049315881c5b1b8a8eba6ecf2bdd40fb9a3ff6
hrpark <hrparkdev@gmail.com>
2025-05-14 22:54:05 +0900

    게시글 추가 및 수정 작업 (리다이렉션 수정)

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 """
 #SSR 방식이라면 jsonify() 대신 redirect()나 render_template()를 써야 함
 
-from flask import Flask, render_template, request, redirect, url_for, session, jsonify
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 from pymongo import MongoClient
 from models.user import create_user, get_user_by_user_id, validate_user_password
 from flask_cors import CORS
@@ -95,51 +95,118 @@ def login():
 
 
 #게시물
-@app.route('/restaurant')  # 등록 페이지
-def restaurant_page():
+@app.route('/post', methods=['GET', 'POST'])
+def post_page():
+    user_id = session.get("user_id")
+    if request.method == 'POST':
+        if user_id is None:
+            flash('로그인이 필요합니다.')
+            return redirect(url_for('post_page'))
+
+        title = request.form.get('title')
+        content = request.form.get('content')
+        address = request.form.get('address')
+
+        post = {
+            'title': title,
+            'user_id': user_id,
+            'content': content,
+            'address': address
+        }
+
+        result = db.jungle.insert_one(post)
+        post_id = str(result.inserted_id)
+        flash('등록이 완료되었습니다.')
+        return redirect(url_for('detail', id=post_id)) 
+        # return redirect(url_for('home'))
+
     return render_template('add_post.html')
 
-@app.route('/restaurant/post', methods=['POST'])
-def create_restaurant():
-    user_id = session.get("user_id")
 
-    if user_id is None:
-      return jsonify({'result': 'fail', 'msg': '로그인이 필요합니다.'}), 401
+@app.route('/post/<id>/edit', methods=['GET', 'POST'])
+def edit_post(id):
+    post = db.jungle.find_one({'_id': ObjectId(id)})
+
+    if request.method == 'POST':
+        title = request.form.get('title')
+        content = request.form.get('content')
+        address = request.form.get('address')
+
+        db.jungle.update_one(
+            {'_id': ObjectId(id)},
+            {'$set': {
+                'title': title,
+                'content': content,
+                'address': address
+            }}
+        )
+
+        flash('수정이 완료되었습니다.')
+        return redirect(url_for('detail', id=post_id))
+        # return redirect(url_for('home'))
+
+    return render_template('edit_post.html', post=post)
+
+
+@app.route('/post/<id>/delete', methods=['POST'])
+def delete_post(id):
+    result = db.jungle.delete_one({'_id': ObjectId(id)})
+    if result.deleted_count == 1:
+        flash('삭제가 완료되었습니다.')
+    else:
+        flash('삭제할 게시글이 존재하지 않습니다.')
+    return redirect(url_for('home'))
+
+
+
+
+
+
+# @app.route('/restaurant')  # 등록 페이지
+# def restaurant_page():
+#     return render_template('add_post.html')
+
+# @app.route('/restaurant/post', methods=['POST'])
+# def create_restaurant():
+#     user_id = session.get("user_id")
+
+#     if user_id is None:
+#       return jsonify({'result': 'fail', 'msg': '로그인이 필요합니다.'}), 401
     
-    title_receive = request.form.get('title_give')
-    content_receive = request.form.get('content_give')
-    address_receive = request.form.get('address_give')
+#     title_receive = request.form.get('title_give')
+#     content_receive = request.form.get('content_give')
+#     address_receive = request.form.get('address_give')
 
-    restaurant = {
-        'title': title_receive,
-        'user_id': user_id,
-        'content': content_receive,
-        'address': address_receive
-    }
+#     restaurant = {
+#         'title': title_receive,
+#         'user_id': user_id,
+#         'content': content_receive,
+#         'address': address_receive
+#     }
 
-    db.restaurant.insert_one(restaurant)
-    return jsonify({'result': 'success'})
+#     db.restaurant.insert_one(restaurant)
+#     return jsonify({'result': 'success'})
 
-@app.route('/restaurant/<id>/edit')  # 수정 페이지
-def edit_restaurant_page(id):
-    restaurant = db.restaurant.find_one({'_id': ObjectId(id)})
-    return render_template('edit_post.html', restaurant=restaurant)
+# @app.route('/restaurant/<id>/edit')  # 수정 페이지
+# def edit_restaurant_page(id):
+#     restaurant = db.restaurant.find_one({'_id': ObjectId(id)})
+#     return render_template('edit_post.html', restaurant=restaurant)
 
-@app.route('/restaurant/<id>/update', methods=['POST'])
-def edit_restaurant(id):
-    title_receive = request.form['title_give']
-    content_receive = request.form['content_give']
-    address_receive = request.form['address_give']
+# @app.route('/restaurant/<id>/update', methods=['POST'])
+# def edit_restaurant(id):
+#     title_receive = request.form['title_give']
+#     content_receive = request.form['content_give']
+#     address_receive = request.form['address_give']
 
-    db.restaurant.update_one(
-        {'_id': ObjectId(id)},
-        {'$set': {
-            'title': title_receive,
-            'content': content_receive,
-            'address': address_receive
-        }}
-    )
-    return jsonify({'result': 'success'})
+#     db.restaurant.update_one(
+#         {'_id': ObjectId(id)},
+#         {'$set': {
+#             'title': title_receive,
+#             'content': content_receive,
+#             'address': address_receive
+#         }}
+#     )
+#     return jsonify({'result': 'success'})
 
 #댓글
 
