a5afe4dcc693c349dbea4755a70c7b9ecb5f9f28
JH-JG <zksk4321@gmail.com>
2025-05-15 10:16:21 +0900

    수정 및 병합

--- app.py
+++ app.py
@@ -4,7 +4,7 @@
 
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from pymongo import MongoClient
-from models.user import create_user, get_user_by_object_id, get_user_by_user_id, validate_user_password
+from models.user import create_user, get_user_by_user_id, validate_user_password, get_user_by_object_id
 from flask_cors import CORS
 import os 
 from bson import ObjectId
@@ -27,7 +27,7 @@ def home():
     return render_template('mainpage.html', info_list=info_list)
 
 
-
+#-----------------보아
 #회원가입
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
@@ -63,8 +63,6 @@ def signup():
     else :
         return render_template('signup.html')
     
-    
-
 
 #로그인
 @app.route('/login', methods=['GET','POST'])
@@ -96,13 +94,92 @@ def login():
         return render_template('login.html')
 
 
+#로그아웃
+@app.route('/logout', methods=['GET'])
+def logout():
+    session.pop('user_id', None)
+    session.pop('user_oid', None)
+    return redirect(url_for('home'))
+
+
+# 북마크 페이지 렌더링 (SSR 방식)
+@app.route('/bookmark', methods=['GET'])
+def bookmark():
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
+
+    user = get_user_by_object_id(db, session['user_oid'])
+    bookmark_ids = user.get('bookmarks', [])
+
+    # ObjectId 리스트로 변환 (MongoDB 쿼리용)
+    try:
+        bookmark_object_ids = [ObjectId(bid) if isinstance(bid, str) else bid for bid in bookmark_ids]
+    except Exception as e:
+        print("ObjectId 변환 실패:", e)
+        bookmark_object_ids = []
+
+    posts = list(db.jungle.find({'_id': {'$in': bookmark_object_ids}}))
+
+    # 템플릿에서 사용할 수 있도록 _id를 문자열로 변환
+    for post in posts:
+        post['_id'] = str(post['_id'])
+
+    return render_template('bookmarkpage.html', posts=posts)
+
+
+#사용자 UX 경험을 고려했을 때 AJAX로 구현하는 것이 좋음
+#SSR로 구현 시 -> 즐겨찾기 누를 때마다 페이지 전체 새로고침 , flash()띄우면 북마크 페이지로 리디렉션되어야만 사용자에게 보여짐 -> 당장 나가고 싶은 UX됨....
+@app.route('/add_bookmark', methods=['POST'])
+def add_bookmark():
+
+    # 로그인 되어 있지 않으면 로그인 페이지로 리디렉션
+    if 'user_id' not in session:
+        return jsonify({'result': 'fail', 'msg': '로그인이 필요합니다.'}), 401
+
+    # 클라이언트로부터 post_id를 받아옴
+    post_id = request.form.get('post_id')
+    if not post_id:
+        return jsonify({'result': 'fail', 'msg': 'post_id가 필요합니다.'}), 400
+
+    # post_id를 ObjectId로 변환 (MongoDB용)
+    try:
+        post_oid = ObjectId(post_id)
+    except Exception:
+        return jsonify({'error': '유효하지 않은 post_id입니다.'}), 400
+
+    # 사용자 정보 불러오기
+    user_oid = session['user_oid']
+    user = db.users.find_one({'_id': ObjectId(user_oid)})
+
+
+    # 북마크 필드가 없으면 빈 리스트로 초기화
+    bookmarks = user.get('bookmarks', [])
+
+
+    # 이미 북마크한 게시물인지 확인
+    if post_oid in bookmarks:
+        return jsonify({'error': '이미 북마크한 게시물입니다.'}), 409
+
+
+    # 중복 없이 북마크 추가
+    db.users.update_one(
+        {'_id': ObjectId(user_oid)},
+        {'$addToSet': {'bookmarks': post_oid}}  # 중복 없이 추가
+    )
+
+
+    return jsonify({'message': '북마크 추가됨'})
+
+
+#------------------- 혜린
 
 #게시물
 @app.route('/post', methods=['GET', 'POST'])
 def post_page():
-    user_id = session.get("user_id")
+    # user_id = session.get("user_id")
+    user_oid = session.get("user_oid")
     if request.method == 'POST':
-        if user_id is None:
+        if user_oid is None:
             flash('로그인이 필요합니다.')
             return redirect(url_for('post_page'))
 
@@ -115,13 +192,20 @@ def post_page():
             img_bytes = file.read()
             img_64 = base64.b64encode(img_bytes).decode('utf-8')
 
-        post = {
-            'title': title,
-            'user_id': user_id,
-            'content': content,
-            'address': address,
-            'image': img_64
-        }
+            post = {
+                'title': title,
+                'user_id': user_oid,
+                'content': content,
+                'address': address,
+                'image': img_64
+            }
+        else:
+            post = {
+                'title': title,
+                'user_id': user_oid,
+                'content': content,
+                'address': address
+            }
 
         result = db.jungle.insert_one(post)
         post_id = str(result.inserted_id)
@@ -179,80 +263,7 @@ def delete_post(id):
     return redirect(url_for('home'))
 
 
-
-#로그아웃
-@app.route('/logout', methods=['GET'])
-def logout():
-    session.pop('user_id', None)
-    session.pop('user_oid', None)
-    return redirect(url_for('home'))
-
-
-
-# 북마크 페이지 렌더링 (SSR 방식)
-@app.route('/bookmark', methods=['GET'])
-def bookmark():
-    # 로그인 검증
-    if 'user_id' not in session:
-        return redirect(url_for('login'))
-    
-    
-    # 사용자 정보 가져오기
-    user = get_user_by_object_id(db, session['user_oid'])
-    
-    
-    # 북마크한 게시물 ID 리스트
-    bookmark_ids = user.get('bookmarks', [])  # 'bookmarks' 필드명 확인!
-    
-    # 북마크한 게시물 전체 조회
-    posts = list(db.posts.find({'_id': {'$in': bookmark_ids}}))
-    
-    # 템플릿에 posts 넘겨서 렌더링
-    return render_template('bookmarkpage.html', posts=posts)
-
-
-#사용자 UX 경험을 고려했을 때 AJAX로 구현하는 것이 좋음
-#SSR로 구현 시 -> 즐겨찾기 누를 때마다 페이지 전체 새로고침 , flash()띄우면 북마크 페이지로 리디렉션되어야만 사용자에게 보여짐 -> 당장 나가고 싶은 UX됨....
-@app.route('/add_bookmark', methods=['POST'])
-def add_bookmark():
-    
-    # 로그인 되어 있지 않으면 로그인 페이지로 리디렉션
-    if 'user_id' not in session:
-        return jsonify({'result': 'fail', 'msg': '로그인이 필요합니다.'}), 401
-    
-    # 클라이언트로부터 post_id를 받아옴
-    post_id = request.form.get('post_id')
-    if not post_id:
-        return jsonify({'result': 'fail', 'msg': 'post_id가 필요합니다.'}), 400
-    
-    # post_id를 ObjectId로 변환 (MongoDB용)
-    try:
-        post_oid = ObjectId(post_id)
-    except Exception:
-        return jsonify({'error': '유효하지 않은 post_id입니다.'}), 400
-    
-    # 사용자 정보 불러오기
-    user_oid = session['user_oid']
-    user = db.users.find_one({'_id': ObjectId(user_oid)})
-    
-    
-    # 북마크 필드가 없으면 빈 리스트로 초기화
-    bookmarks = user.get('bookmarks', [])
-    
-    
-    # 이미 북마크한 게시물인지 확인
-    if post_oid in bookmarks:
-        return jsonify({'error': '이미 북마크한 게시물입니다.'}), 409
-    
-    
-    # 중복 없이 북마크 추가
-    db.users.update_one(
-        {'_id': ObjectId(user_oid)},
-        {'$addToSet': {'bookmarks': post_oid}}  # 중복 없이 추가
-    )
-    return jsonify({'message': '북마크 추가됨'})
-
-
+#---------------주형
 
 #상세 페이지
 @app.route('/detail/<post_id>')
